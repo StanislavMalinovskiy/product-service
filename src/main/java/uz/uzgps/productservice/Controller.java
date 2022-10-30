1fbb356ba8866ef109b7090474141229014829ec
stanislav <@Xy3449439>
2022-10-31 02:50:11 +0500

    Remove crossorigin on localhost

--- src/main/java/uz/uzgps/productservice/Controller.java
+++ src/main/java/uz/uzgps/productservice/Controller.java
@@ -1,5 +1,6 @@
 package uz.uzgps.productservice;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,14 +8,23 @@ import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
 
 @RestController
+@CrossOrigin()
 public class Controller {
 
     private static final String USER_ROLE_PRODUCT = "product";
 
     @GetMapping("/product")
     @RolesAllowed({USER_ROLE_PRODUCT})
-    public String getProduct(Principal principal) {
+    @CrossOrigin
+    public String getProduct() {
 
-        return "Response from Product Service, User Id:" + principal.getName();
+        return "Response from Product Service, User Id:";
+    }
+    @GetMapping("/product2")
+    @RolesAllowed({USER_ROLE_PRODUCT})
+
+    public String getProduct2(Principal principal) {
+
+        return "Response from Product2 Service, User Id:" + principal.getName();
     }
 }
